b3297ecf9b8232ea019466c81e871680c4d2ef2e
prashant <prashant.tongra@pickrr.com>
2021-11-19 01:07:16 +0530

    fix:issue

--- src/components/ProductDetails/index.js
+++ src/components/ProductDetails/index.js
@@ -1,40 +1,41 @@
-import React, {useContext} from 'react';
-import { Row, Col } from 'antd';
+import React, { useContext } from "react";
+import { Row, Col } from "antd";
 
-import {ProductDetailContainer} from './style';
-import ProductCard from './ProductCard';
-import { DataContext } from '../../context/dataProvider';
+import { ProductDetailContainer } from "./style";
+import ProductCard from "./ProductCard";
+import { DataContext } from "../../context/dataProvider";
 
-const ProductDetails =() =>{
+const ProductDetails = () => {
+  const { brandDataState } = useContext(DataContext);
 
-    const { brandDataState } = useContext(DataContext);
- 
-    const { brandData:{ 
-            other: {
-                    product_details =[], 
-                    other_details = {}
-                } ={} 
-            } = {}
-        } = brandDataState;
+  const {
+    brandData: {
+      other: { product_details = [], other_details = {} } = {},
+    } = {},
+  } = brandDataState;
 
-    const {brandData ={}} = brandDataState;    
+  const { brandData = {} } = brandDataState;
 
-    return(
-        <ProductDetailContainer height={!Object.keys(brandData).length ? true : false}>
-            <Row gutter={[24, 28]} style={{justifyContent: 'center'}}>
-                {
-                    product_details.map((productData, index) =>{
-                        return(                          
-                            productData && (
-                            <Col key={index} xl={8} lg={8} md={12} sm={24} xs={24}> 
-                                <ProductCard productData={productData} otherDetails={other_details} />
-                            </Col>
-                            )
-                        )
-                    })
-                }
-            </Row>   
-        </ProductDetailContainer> 
-    )
-}
+  return (
+    <ProductDetailContainer
+      height={!Object.keys(brandData).length ? true : false}
+    >
+      <Row gutter={[24, 28]} style={{ justifyContent: "center" }}>
+        {product_details.length &&
+          product_details.map((productData, index) => {
+            return (
+              productData && (
+                <Col key={index} xl={8} lg={8} md={12} sm={24} xs={24}>
+                  <ProductCard
+                    productData={productData}
+                    otherDetails={other_details}
+                  />
+                </Col>
+              )
+            );
+          })}
+      </Row>
+    </ProductDetailContainer>
+  );
+};
 export default ProductDetails;
