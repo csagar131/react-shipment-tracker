c8c3119708a6089f707a99befb1fdf05b1f41d53
prashant <prashant.tongra@pickrr.com>
2021-11-30 14:16:28 +0530

    update

--- src/components/Overview/index.js
+++ src/components/Overview/index.js
@@ -87,17 +87,7 @@ const Overview = () => {
   return (
     <>
       <Row style={{ justifyContent: 'center', width: '100%' }}>
-        <Col
-          lg={
-            company_name === 'bellavita' || company_name === 'OZiva' ? 15 : 18
-          }
-          xl={
-            company_name === 'bellavita' || company_name === 'OZiva' ? 15 : 18
-          }
-          md={22}
-          sm={22}
-          xs={22}
-        >
+        <Col lg={15} xl={15} md={22} sm={22} xs={22}>
           <TrackCard>
             <SpaceBetweenContainer
               style={{
@@ -209,16 +199,8 @@ const Overview = () => {
             }}
           >
             <Col
-              lg={
-                company_name === 'bellavita' || company_name === 'OZiva'
-                  ? 15
-                  : 18
-              }
-              xl={
-                company_name === 'bellavita' || company_name === 'OZiva'
-                  ? 15
-                  : 18
-              }
+              lg={15}
+              xl={15}
               md={22}
               sm={22}
               xs={22}
@@ -237,12 +219,8 @@ const Overview = () => {
           </Row>
         ) : err ? (
           <Col
-            lg={
-              company_name === 'bellavita' || company_name === 'OZiva' ? 15 : 18
-            }
-            xl={
-              company_name === 'bellavita' || company_name === 'OZiva' ? 15 : 18
-            }
+            lg={15}
+            xl={15}
             // sm={{ span: 22, offset: 1 }}
             // xs={{ span: 22, offset: 1 }}
             md={22}
@@ -261,17 +239,7 @@ const Overview = () => {
             // )} */}
           </Col>
         ) : brandErr ? (
-          <Col
-            lg={
-              company_name === 'bellavita' || company_name === 'OZiva' ? 15 : 18
-            }
-            xl={
-              company_name === 'bellavita' || company_name === 'OZiva' ? 15 : 18
-            }
-            sm={22}
-            xs={22}
-            style={{ minHeight: '58vh' }}
-          >
+          <Col lg={15} xl={15} sm={22} xs={22} style={{ minHeight: '58vh' }}>
             <Alert
               message={brandErr && brandErr.err}
               type="error"
@@ -279,17 +247,7 @@ const Overview = () => {
             />
           </Col>
         ) : (
-          <Col
-            lg={
-              company_name === 'bellavita' || company_name === 'OZiva' ? 15 : 18
-            }
-            xl={
-              company_name === 'bellavita' || company_name === 'OZiva' ? 15 : 18
-            }
-            sm={22}
-            xs={22}
-            style={{ minHeight: '58vh' }}
-          >
+          <Col lg={15} xl={15} sm={22} xs={22} style={{ minHeight: '58vh' }}>
             {/* <Alert
           message={brandErr && brandErr.err}
           type="error"
